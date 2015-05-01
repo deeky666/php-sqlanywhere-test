400194f0e9a7ca406fa78de7319428f9edef3ab5
Steve Müller <st.mueller@dzh-online.de>
2015-05-01 12:59:06 +0200

    test

--- setup.sh
+++ setup.sh
@@ -4,5 +4,13 @@ export SQLANY16=`pwd`/sqlanywhere16
 export LD_LIBRARY_PATH=`pwd`/sqlanywhere16/lib64
 
 wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xz
+wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLQnlZZkdMa20zdkU" | tar xz
+
+cd php-sqlanywhere
+./configure
+make
+make install
+
+php -m
 
 ./sqlanywhere16/bin64/dbsrv16 -ud -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
