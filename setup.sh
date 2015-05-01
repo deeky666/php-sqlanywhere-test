ca843501c8f408f93276ddd1c042a692d7614299
Steve Müller <st.mueller@dzh-online.de>
2015-05-01 12:38:42 +0200

    test

--- setup.sh
+++ setup.sh
@@ -5,8 +5,4 @@ export LD_LIBRARY_PATH=`pwd`/sqlanywhere16/lib64
 
 wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xz
 
-echo $SQLANY16
-echo $LD_LIBRARY_PATH
-pwd
-
-./sqlanywhere16/bin64/dbsrv16 -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
+./sqlanywhere16/bin64/dbsrv16 -ud -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
