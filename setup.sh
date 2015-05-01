67c3ca386a7cd9c049a5d3af30802d1da08abada
Steve Müller <st.mueller@dzh-online.de>
2015-05-01 12:05:00 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 export SQLANY16=$HOME/sqlanywhere16
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/sqlanywhere16/lib64
 
 wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xz
 
 echo "$HOME/sqlanywhere16/lib64" > /etc/ld.so.conf.d/sqlanywhere.conf
 ldconfig
+
+echo $SQLANY16
+echo $LD_LIBRARY_PATH
 
 ./sqlanywhere16/bin64/dbsrv16 -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
