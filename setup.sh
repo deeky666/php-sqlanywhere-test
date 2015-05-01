dc607af0d498d26354cf3692029d158a2f00edc5
Steve Müller <st.mueller@dzh-online.de>
2015-05-01 11:59:27 +0200

    test

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 export SQLANY16=$HOME/sqlanywhere16
 
-wget "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xzf
+wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xz
 
 echo "$HOME/sqlanywhere16/lib64" > /etc/ld.so.conf.d/sqlanywhere.conf
 ldconfig
