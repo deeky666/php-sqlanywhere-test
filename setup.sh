b7539bf300ed0bd0f90c267b22f8213c38f6ddc7
Steve Müller <st.mueller@dzh-online.de>
2015-05-01 14:49:53 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-
-
 export SQLANY16=`pwd`/sqlanywhere16
 export LD_LIBRARY_PATH=`pwd`/sqlanywhere16/lib64
 
-wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xz
-wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLa3k0dVlpcE00Tk0" | tar xz
+apt-get update -y
+apt-get install -y php5-dev  --no-install-recommends
 
-echo "extension = `pwd`/php_sqlanywhere/sqlanywhere_$(phpenv version-name).so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLV1ZZdDBvSjFPZFU" | tar xz
+wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLdmxScC1TR3ZRaTQ" | tar xz
+
+cd php_sqlanywhere
+phpize
+./configure
+make -j`nproc`
+make install
+
+echo "extension = sqlanywhere.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 php -m
-
-echo "extension = `pwd`/php_sqlanywhere/sqlanywhere_$(phpenv version-name).so"
-echo ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 ./sqlanywhere16/bin64/dbsrv16 -ud -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
