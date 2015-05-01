4ee1ddde79f51ac08307690e23c0b7fe45ea6661
Steve Müller <st.mueller@dzh-online.de>
2015-05-01 11:39:14 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-export SQLANY16=/opt/sqlanywhere16
-apt-get update -y
-apt-get install -y expect --no-install-recommends
-wget http://d5d4ifzqzkhwt.cloudfront.net/sqla16developer/bin/sqla16developerlinux.tar.gz
-tar xf sqla16developerlinux.tar.gz --strip 2
-expect setup16.expect
-echo "/opt/sqlanywhere16/lib64" > /etc/ld.so.conf.d/sqlanywhere.conf
+export SQLANY16=$HOME/sqlanywhere16
+
+wget http://d5d4ifzqzkhwt.cloudfront.net/sqla16developer/bin/sqla16developerlinux.tar.gz | tar xzf
+
+echo "$HOME/sqlanywhere16/lib64" > /etc/ld.so.conf.d/sqlanywhere.conf
 ldconfig
 
-cd /opt/sqlanywhere16
-
-/opt/sqlanywhere/bin64/dbsrv16 -x tcpip{DOBROADCAST=NO} demo.db
+./sqlanywhere16/bin64/dbsrv16 -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
