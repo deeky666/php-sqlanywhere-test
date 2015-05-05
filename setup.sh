86f4d2677a24743d9b1d10799e8ba99a72c5ef36
Steve Müller <st.mueller@dzh-online.de>
2015-05-05 14:57:20 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLWUUzY2lTcmVjb1k" | tar xz # server
-wget -qO- "https://drive.google.com/uc?export=download&id=$SQLANYWHERE_GDRIVE_ID" | tar xz # server
+wget -qO- "https://drive.google.com/uc?export=download&id=$SQLANYWHERE_GDRIVE_ID" | tar xz # SQL Anywhere server
 wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLS1oxaUQzTy1Jczg" | tar xz # PHP extensions
 
 echo "extension = `pwd`/php_sqlanywhere/sqlanywhere_$(phpenv version-name).so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
-php -m
-
-./sqlanywhere16/bin64/dbsrv16 -ud -x tcpip{DOBROADCAST=NO} ./sqlanywhere16/demo.db
+./sqlanywhere/bin64/dbsrv -ud -x tcpip{DOBROADCAST=NO} ./sqlanywhere/demo.db
