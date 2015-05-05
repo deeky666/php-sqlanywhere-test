b62c4dc49a1b872bd77e717f946966c8f04dafde
Steve Müller <st.mueller@dzh-online.de>
2015-05-05 14:40:35 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#export SQLANY16=`pwd`/sqlanywhere16
-#export LD_LIBRARY_PATH=`pwd`/sqlanywhere16/lib64
-
-wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLWUUzY2lTcmVjb1k" | tar xz
-wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLS1oxaUQzTy1Jczg" | tar xz
+#wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLWUUzY2lTcmVjb1k" | tar xz # server
+wget -qO- "https://drive.google.com/uc?export=download&id=$SQLANYWHERE_GDRIVE_TOKEN" | tar xz # server
+wget -qO- "https://drive.google.com/uc?export=download&id=0BxDG3LHQ2MkLS1oxaUQzTy1Jczg" | tar xz # PHP extensions
 
 echo "extension = `pwd`/php_sqlanywhere/sqlanywhere_$(phpenv version-name).so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
